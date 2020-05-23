3a665da801d5d4703903069b0cfdcdfd1b8db11b
ZAFER OZCAN <40546836+zaferozzcan@users.noreply.github.com>
2020-05-23 14:18:54 -0400

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import CreateArea from "./CreateArea";
+import CreateNoteArea from "./CreateNoteArea";
 
 function App() {
   const [notes, setNotes] = useState([]);
